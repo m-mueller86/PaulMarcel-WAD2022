4f961fd9f99e373731ba678bf4a7330e72a33f6a
m-mueller86 <s0570833@htw-berlin.de>
2022-11-03 14:35:26 +0100

    map added, some css added

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([52.51303, 13.4144269], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
