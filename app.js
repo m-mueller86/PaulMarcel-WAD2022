5bd7b3e435a91f49969271d0b97de1e927065dd5
m-mueller86 <s0570833@htw-berlin.de>
2023-01-07 11:19:20 +0100

    add location happy path works

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
 
-app.post("/users", function(req, res) {
+app.post("/users", function (req, res) {
 
     let username = req.body.username;
     let password = req.body.password;
@@ -19,8 +19,8 @@ app.post("/users", function(req, res) {
         function (err, client) {
             if (err) throw err;
             let db = client.db("susLocDB");
-            db.collection("users").findOne({userId: username},
-                function(err, result) {
+            db.collection("users").findOne({ userId: username },
+                function (err, result) {
                     if (err) throw err;
                     if (result !== null && password === result.password) {
                         delete result.password;
@@ -29,6 +29,44 @@ app.post("/users", function(req, res) {
                         res.status(401);
                         res.end();
                     }
+                    client.close();
+                });
+        });
+});
+
+app.post("/susLocs", function (req, res) {
+
+    let location = {
+        lat: req.body.lat,
+        lon: req.body.lon,
+        name: req.body.name,
+        address: req.body.address,
+        number: req.body.number,
+        postcode: req.body.postcode,
+    }
+
+    MongoClient.connect(url,
+        function (err, client) {
+            if (err) throw err;
+            let db = client.db("susLocDB");
+            db.collection("locations").insertOne(location,
+                function (err, result) {
+                    if (err) throw err;
+                    client.close();
+                });
+
+        });
+});
+
+app.get("/susLocs", function (req, res) {
+    MongoClient.connect(url,
+        function (err, client) {
+            if (err) throw err;
+            let db = client.db("susLocDB");
+            db.collection("locations").find({}).toArray(
+                function (err, result) {
+                    if (err) throw err;
+                    res.status(200).send(result);
                     client.close();
                 });
         });
