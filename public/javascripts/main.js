3a35f21a7697c62ae5bee5fbf29ef889ba0269f1
m-mueller86 <s0570833@htw-berlin.de>
2023-01-09 14:20:06 +0100

    layerGroup added and refactor main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,20 @@
 window.onload = initialSetup;
 
+const loginForm = document.getElementById("login-form");
+const logoutButton = document.getElementById("logout-button");
+const addForm = document.getElementById("add-form");
+const addButton = document.getElementById("add-button");
+const cancelAdd = document.getElementById("cancel-add");
+const cancelUpdate = document.getElementById("cancel-update");
+const addLocation = document.getElementById("add-location");
+const deleteLocation = document.getElementById("update-delete-form");
+const updateLocation = document.getElementById("update-delete-form");
+const impressum = document.getElementById("impressum");
+const back = document.getElementById("back");
+
+var map = L.map('map');
+var markers = L.layerGroup();
+
 function initialSetup() {
     document.getElementById("impressum-page").style.display = "none";
     document.getElementById("mainpage").style.display = "none";
@@ -17,8 +32,6 @@ function initialSetup() {
     addEventListenerToLocationsElements();
 }
 
-var map = L.map('map');
-
 function initializeMap() {
     // Map with coordinates for start view
     map.setView([52.51303, 13.4144269], 12);
@@ -27,8 +40,47 @@ function initializeMap() {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
 }
 
+function loginAsAdmina() {
+    document.getElementById("mainpage").style.display = "block";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("add-button").style.display = "block";
+    document.getElementById("welcome-message").innerText = `Welcome back ${loginForm.username.value}`;
+    document.getElementById("update").style.display = "block";
+    document.getElementById("delete").style.display = "block";
+    document.getElementById("locationId").style.display = "block";
+
+    initializeMap();
+}
+
+function loginAsGuest() {
+    document.getElementById("mainpage").style.display = "block";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("add-button").style.display = "none";
+    document.getElementById("welcome-message").innerText = `Welcome back ${loginForm.username.value}`;
+    document.getElementById("update").style.display = "none";
+    document.getElementById("delete").style.display = "none";
+    document.getElementById("locationId").style.display = "none";
+
+    initializeMap();
+}
+
+function refreshMap() {
+
+    markers.clearLayers();
+    document.getElementById("location-list").innerHTML = "";
+
+    fetch("/susLocs")
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(location => {
+                location.marker = L.marker([location.lat, location.lon])
+                addToMap(location);
+            })
+        });
+}
 
 async function getLongitudeLatitude(locationName, locationNumber, locationAddress, locationPostcode) {
     const url = "http://nominatim.openstreetmap.org/search?street=" + locationNumber + "+" + locationAddress +
@@ -37,29 +89,51 @@ async function getLongitudeLatitude(locationName, locationNumber, locationAddres
     let response = await fetch(url);
     let result = await response.json();
 
-    let location = {
-        lat: result[0]["lat"],
-        lon: result[0]["lon"],
-        name: locationName,
-        address: locationAddress,
-        number: locationNumber,
-        postcode: locationPostcode
-    }
+    if (result.length === 0) {
+        alert("invalid address!");
+    } else {
 
-    return location;
+        let location = {
+            lat: result[0]["lat"],
+            lon: result[0]["lon"],
+            name: locationName,
+            address: locationAddress,
+            number: locationNumber,
+            postcode: locationPostcode
+        }
+
+        return location;
+    }
 }
 
-const loginForm = document.getElementById("login-form");
-const logoutButton = document.getElementById("logout-button");
-const addForm = document.getElementById("add-form");
-const addButton = document.getElementById("add-button");
-const cancelAdd = document.getElementById("cancel-add");
-const cancelUpdate = document.getElementById("cancel-update");
-const addLocation = document.getElementById("add-location");
-const deleteLocation = document.getElementById("update-delete-form");
-const updateLocation = document.getElementById("update-delete-form");
-const impressum = document.getElementById("impressum");
-const back = document.getElementById("back");
+function addToMap(location) {
+    markers.addLayer(location.marker).addTo(map);
+    document.getElementById("location-list").innerHTML += `<li id="${location.name}">` + location.name + "</li><br>";
+    location.marker.bindPopup(location.name);
+    setTimeout(addEventListenerToLocationsElements, 300);
+}
+
+function addEventListenerToLocationsElements() {
+
+    fetch("/susLocs")
+        .then(res => res.json())
+        .then(data => data.forEach(location => {
+            document.getElementById(location.name).addEventListener("click", (e) => {
+                document.getElementById("mainpage").style.display = "none";
+                document.getElementById("updateDelete").style.display = "block";
+                document.getElementById("street").innerHTML = `<input type="text" value="${location.address}" name="street" required>` +
+                    "<label>Straße</label>";
+                document.getElementById("housenumber").innerHTML = `<input type="text" value="${location.number}" name="housenumber" required>` +
+                    "<label>Nr.</label";
+                document.getElementById("locationname").innerHTML = `<input type="text" value="${location.name}" name="name" required>` +
+                    "<label>Name</label>";
+                document.getElementById("plz").innerHTML = `<input type="text" value="${location.postcode}" name="postcode" required>` +
+                    "<label>PLZ</label>";
+                document.getElementById("locationId").innerHTML = `<input type="text" value="${location._id}" name="locationId" required>` +
+                    "<label>ID</label>"
+            });
+        }));
+}
 
 loginForm.addEventListener("submit", (e) => {
 
@@ -82,9 +156,9 @@ loginForm.addEventListener("submit", (e) => {
             }
         })
         .then(data => {
-            if (data.isAdmin === "true") {
+            if (data !== undefined && data.isAdmin === "true") {
                 loginAsAdmina();
-            } else if (data.isAdmin === "false") {
+            } else if (data !== undefined && data.isAdmin === "false") {
                 loginAsGuest();
             }
         })
@@ -92,29 +166,6 @@ loginForm.addEventListener("submit", (e) => {
 
 });
 
-function loginAsAdmina() {
-    document.getElementById("mainpage").style.display = "block";
-    document.getElementById("login").style.display = "none";
-    document.getElementById("add-button").style.display = "block";
-    document.getElementById("welcome-message").innerText = `Welcome back ${loginForm.username.value}`;
-    document.getElementById("update").style.display = "block";
-    document.getElementById("delete").style.display = "block";
-
-    initializeMap();
-}
-
-function loginAsGuest() {
-    document.getElementById("mainpage").style.display = "block";
-    document.getElementById("login").style.display = "none";
-    document.getElementById("add-button").style.display = "none";
-    document.getElementById("welcome-message").innerText = `Welcome back ${loginForm.username.value}`;
-    document.getElementById("update").style.display = "none";
-    document.getElementById("delete").style.display = "none";
-
-    initializeMap();
-}
-
-// Logout
 logoutButton.addEventListener("click", (e) => {
     document.getElementById("mainpage").style.display = "none";
     document.getElementById("login").style.display = "block";
@@ -122,7 +173,6 @@ logoutButton.addEventListener("click", (e) => {
     document.getElementById("username-field").value = "";
 });
 
-// Adding location
 addButton.addEventListener("click", (e) => {
     document.getElementById("mainpage").style.display = "none";
     document.getElementById("add").style.display = "block";
@@ -158,56 +208,12 @@ addForm.addEventListener("submit", (e) => {
         });
 
 });
-function addToMap(location) {
-    location.marker.addTo(map);
-    document.getElementById("location-list").innerHTML += `<li id="${location.name}">` + location.name + "</li><br>";
-    location.marker.bindPopup(location.name);
-}
 
-function addToLocations(lat, lon, name, address, number, postcode) {
-    let location = {
-        name: name,
-        address: address,
-        number: number,
-        postcode: postcode,
-        lat: lat,
-        lon: lon,
-        marker: L.marker([lat, lon])
-    };
-
-    locations.push(location);
-    addToMap(location);
-    addEventListenerToLocationsElements();
-    localStorage.setItem('locations', JSON.stringify(locationsWithoutMarker()));
-}
-
-// Update/Delete
 cancelUpdate.addEventListener("click", (e) => {
     document.getElementById("updateDelete").style.display = "none";
     document.getElementById("mainpage").style.display = "block";
-    addEventListenerToLocationsElements();
 });
 
-function addEventListenerToLocationsElements() {
-
-    fetch("/susLocs")
-        .then(res => res.json())
-        .then(data => data.forEach(location => {
-            document.getElementById(location.name).addEventListener("click", (e) => {
-                document.getElementById("mainpage").style.display = "none";
-                document.getElementById("updateDelete").style.display = "block";
-                document.getElementById("street").innerHTML = `<input type="text" value="${location.address}" name="street" required>` +
-                    "<label>Straße</label>";
-                document.getElementById("housenumber").innerHTML = `<input type="text" value="${location.number}" name="housenumber" required>` +
-                    "<label>Nr.</label";
-                document.getElementById("locationname").innerHTML = `<input type="text" value="${location.name}" name="name" required>` +
-                    "<label>Name</label>";
-                document.getElementById("plz").innerHTML = `<input type="text" value="${location.postcode}" name="postcode" required>` +
-                    "<label>PLZ</label>";
-            });
-        }));
-
-}
 deleteLocation.addEventListener("reset", (e) => {
 
     const id = {
@@ -263,7 +269,6 @@ updateLocation.addEventListener("submit", (e) => {
         });
 })
 
-// Impressum
 impressum.addEventListener("click", (e) => {
     document.getElementById("mainpage").style.display = "none";
     document.getElementById("add").style.display = "none";
@@ -276,23 +281,3 @@ back.addEventListener("click", (e) => {
     document.getElementById("impressum-page").style.display = "none";
     document.getElementById("login").style.display = "block";
 })
-
-// array of locations
-let locations = [];
-
-function locationsWithoutMarker() {
-    let locationsWithoutMarker = [];
-
-    locations.forEach(location => {
-        let newLocation = {
-            name: location.name,
-            address: location.address,
-            number: location.number,
-            postcode: location.postcode,
-            lat: location.lat,
-            lon: location.lon
-        }
-        locationsWithoutMarker.push(newLocation);
-    });
-    return locationsWithoutMarker;
-}
