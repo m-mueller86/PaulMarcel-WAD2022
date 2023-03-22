b61b1bc7dd70e31587bb838a4ab12efb1e6067d9
m-mueller86 <s0570833@htw-berlin.de>
2023-03-22 18:22:30 +0100

    got rid of unnecessary stuff

--- frontend/src/components/Aside/Aside.js
+++ frontend/src/components/Aside/Aside.js
@@ -1,24 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import "./Aside.css"
 
 const Aside = ({ locations, handleLocationHover }) => {
-    const [hoveredLocation, setHoveredLocation] = useState(null);
-
-    const handleLocationHoverLocal = (location) => {
-        setHoveredLocation(location);
-        handleLocationHover(location);
-    }
-
+    
     return (
         <aside>
             <ul>
                 {locations.map((location) => (
                     <li
                         key={location._id}
-                        onMouseEnter={() => handleLocationHoverLocal(location)}
-                        onMouseLeave={() => handleLocationHoverLocal(null)}
+                        onMouseEnter={() => handleLocationHover(location)}
+                        onMouseLeave={() => handleLocationHover(null)}
                     >
                         <Link
                             to={`/details?id=${location._id}&name=${location.name}&street=${location.address}&housenumber=${location.number}&plz=${location.postcode}`}
